68a40efc00ee6e3ee6b3ea4f79de923a531c7600
tbriand <briandthibaud99@gmail.com>
2022-03-20 15:34:53 +0100

    [add] Friendship command

--- src/api/friendship/cqrs/command/accept-friendship-request.command.ts
+++ src/api/friendship/cqrs/command/accept-friendship-request.command.ts
@@ -3,6 +3,6 @@ import { User } from '../../../user/domain/entities/user.entity';
 export class AcceptFriendshipRequestCommand {
   constructor(
     public readonly friendOne: User,
-    public readonly friendTwo: string,
+    public readonly friendTwo: User,
   ) {}
 }
--- src/api/friendship/cqrs/command/send-friendship-request.command.ts
+++ src/api/friendship/cqrs/command/send-friendship-request.command.ts
@@ -1,5 +1,5 @@
 import { User } from '../../../user/domain/entities/user.entity';
 
 export class SendFriendshipRequestCommand {
-  constructor(public readonly sender: User, public readonly userId: string) {}
+  constructor(public readonly sender: User, public readonly user: User) {}
 }
