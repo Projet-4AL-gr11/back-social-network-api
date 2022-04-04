6f016d787b35de4b7ed7554e4f494bee2241f0fb
tbriand <briandthibaud99@gmail.com>
2022-04-04 12:40:37 +0200

    [add] event

---
+++ src/api/report/cqrs/event/create-report-event.event.ts
@@ -0,0 +1,6 @@
+export class CreateReportEventEvent {
+  constructor(public readonly userId: string,
+              public readonly reportId: string,
+              public readonly id: string) {
+  }
+}
---
+++ src/api/report/cqrs/event/create-report-user.event.ts
@@ -0,0 +1,5 @@
+export class CreateReportUserEvent {
+  constructor(public readonly userId: string,
+              public readonly reportId: string,
+              public readonly id: string) {}
+}
